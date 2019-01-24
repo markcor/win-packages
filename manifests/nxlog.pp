821fe76a9a8ce9234108ca3835100371a6eb7cb4
markcor <mcornmesser@mozilla.com>
2019-01-24 12:34:32 -0800

    admin packages

---
+++ manifests/nxlog.pp
@@ -0,0 +1,20 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class win_packages::nxlog {
+
+$programfilesx86 = $facts['programfilesx86']
+
+    defined_classes::pkg::win_msi_pkg  { "NxLog-CE":
+        pkg             => "nxlog-ce-2.8.1248.msi",
+        install_options => ['/quiet'],
+	}
+
+	file { "$programfilesx86\\nxlog\\cert\\papertrail-bundle.pem":
+		content => file("win_packages\\papertrail-bundle.pem"),
+	}
+	file { "$programfilesx86\\nxlog\\conf\\nxlog.conf":
+		content => epp("win_packages\\nxlog.conf.epp"),
+	)
+}
---
+++ manifests/process_debug.pp
@@ -0,0 +1,16 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class win_packages::process_debug {
+	defined_classes::pkg::win_zip_pkg { 'proc_expolorer':
+		pkg         => 'ProcessExplorer.zip',
+		creates     => "C:\\ProcessExplorer\\procexp.exe",
+		destination => "C:\\ProcessExplorer",
+	}
+	defined_classes::pkg::win_zip_pkg { 'proc_monitor':
+		pkg         => 'ProcessMonitor.zip',
+		creates     => "C:\\ProcessMonitor\\Procmon.exe",
+		destination => "C:\\ProcessMonitor",
+	}
+}
