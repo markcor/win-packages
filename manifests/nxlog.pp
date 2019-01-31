dea40be707b3fac0ed5b6532231d33af51b03bef
markcor <mcornmesser@mozilla.com>
2019-01-31 14:43:08 -0800

    :

--- manifests/nxlog.pp
+++ manifests/nxlog.pp
@@ -6,28 +6,29 @@ class win_packages::nxlog {
 
 $programfilesx86 = $facts['programfilesx86']
 
-   # defined_classes::pkg::win_msi_pkg  { "NxLog-CE":
-    #    pkg             => "nxlog-ce-2.8.1248.msi",
-     #   install_options => ['/quiet'],
-	#}
-	#windows_firewall::exception { 'nxlog':
-  	#	ensure       => present,
-  	#	direction    => 'out',
-  	#	action       => 'allow',
-  	#	enabled      => true,
-  	#	protocol     => 'TCP',
-  	#	local_port   => 514,
-  	#	display_name => 'Nxlog out',
-  	#	description  => 'Nxlogout. [TCP 514]',
-	#}
-#	file { "$programfilesx86\\nxlog\\cert\\papertrail-bundle.pem":
-#		content => file("win_packages/papertrail-bundle.pem"),
-#	}
+	defined_classes::pkg::win_msi_pkg  { "NxLog-CE":
+        pkg             => "nxlog-ce-2.10.2150.msi",
+       install_options => ['/quiet'],
+	}
+	windows_firewall::exception { 'nxlog':
+  		ensure       => present,
+  		direction    => 'out',
+  		action       => 'allow',
+  		enabled      => true,
+  		protocol     => 'TCP',
+  		local_port   => 514,
+  		display_name => 'Nxlog out',
+  		description  => 'Nxlogout. [TCP 514]',
+	}
+	file { "$programfilesx86\\nxlog\\cert\\papertrail-bundle.pem":
+		content => file("win_packages/papertrail-bundle.pem"),
+	}
 	file { "$programfilesx86\\nxlog\\conf\\nxlog.conf":
 		content => epp("win_packages/nxlog.conf.epp"),
 	}
 	service { "nxlog":
 		restart   => true,
+		ensure    => running,
 		subscribe => File["$programfilesx86\\nxlog\\conf\\nxlog.conf"],
 	}
 }
